ade3daf1abb8922d4195a9559f1e9cb1c7882b15
JuchokJuk <juk.juchok@yandex.ru>
2025-01-05 21:02:09 +0300

    export calc and calcSize wrappers

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,5 @@ export { abs, sign } from "./functions/math/signRelated.ts";
 export { round, mod, rem } from "./functions/math/steppedValue.ts";
 export { sin, cos, tan, asin, acos, atan, atan2 } from "./functions/math/trigonometric.ts";
 export { env, variable } from "./functions/reference/variables.ts";
+
+export { calc, calcSize } from "./calc.ts";
